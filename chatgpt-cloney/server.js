42796044425bd5142d0d86e3a84081c0c227afa9
SeanAllen19 <saallen19.dev@gmail.com>
2023-04-26 15:54:38 -0500

    created a server.js file

---
+++ chatgpt-cloney/server.js
@@ -0,0 +1,39 @@
+const PORT = 3000;
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = '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'
+
+app.post('/completions',async(res,req) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role:"user", content: "Hello, how are you?"}],
+                max_tokens: 100,
+            
+        })
+    }
+
+    try {   
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+
+        const data = await response.json()
+        res.send(data)
+    } catch {err} {
+        console.error(err)
+    }
+}) 
+
+app.listen(PORT, () => console.log(`Your server is being run on PORT ${PORT}`));
+
+
--- chatgpt-cloney/src/App.js
+++ chatgpt-cloney/src/App.js
@@ -1,35 +1,61 @@
 const App = () => {
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body : JSON.stringify({
+        message: "Hi world, how are you?",
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    try {
+      const response = await fetch('http://localhost:3000/completions', options)
+      const data = await response.json()
+      console.log(data)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div className="app">
-        <section className="side-bar">
-          <button>+ New Chat</button>
-          <ul className="history">
-            <li>Question 1</li>
-            <li>Question 2</li>
-          </ul>
-          <nav>
-            <p>Made by Sean Allen</p>
-          </nav>
-        </section>
+      <section className="side-bar">
 
-        <section className="main">
-          <h1>CloneyGPT</h1>
-          <ul className="feed">
-          </ul>
+        <button>+ New Chat</button>
 
-          <div className="bottom-section"> 
-            
-            <div className="input-container">
-                <input/>
-                <div id="submit">➢</div>
-            </div>
-            <p className="info">
-            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. Our goal (Including myself) is to make AI systems more natural and safe to interact with. Your feedback will help us improve.
-            </p>
+        <ul className="history">
+          <li>Question 1</li>
+          <li>Question 2</li>
+          <li>Question 3</li>
+        </ul>
 
+        <nav>
+          <p>Made by Sean Allen</p>
+        </nav>
+
+      </section>
+
+      <section className="main">
+        <h1>CloneyGPT</h1>
+        <ul className="feed">
+          
+        </ul>
+
+        <div className="bottom-section">
+
+          <div className="input-container">
+            <input />
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. Our goal (Including myself) is to make AI systems more natural and safe to interact with. Your feedback will help us improve.
+          </p>
 
-        </section>
+        </div>
+
+      </section>
     </div>
   );
 }
